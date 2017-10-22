c3fcbabf8eeff0c752fc15a57c262476ba005da4
kyohei-shimada <kyohei-shimada@users.noreply.github.com>
2017-10-22 20:02:24 +0900

    Revert "クソクエリ除去"

--- app.rb
+++ app.rb
@@ -397,13 +397,15 @@ class App < Sinatra::Base
   end
 
   def get_channel_list_info(focus_channel_id = nil)
-    statement = db.prepare('SELECT * FROM channel WHERE id = ?')
-    channel = statement.execute(focus_channel_id).first
-    if channel
-      [[channel], channel['description']]
-    else
-      [[], '']
+    channels = db.query('SELECT * FROM channel ORDER BY id').to_a
+    description = ''
+    channels.each do |channel|
+      if channel['id'] == focus_channel_id
+        description = channel['description']
+        break
+      end
     end
+    [channels, description]
   end
 
   def ext2mime(ext)
